9720e689f2f9689013a2a3aa6f24ddcb8f274277
Eduardo Barbosa <dev_eduardo@hotmail.com>
2026-09-25 17:47:43 -0300

    update

---
+++ for-loop.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+int main() {
+
+    for (int index = 0; index < 100; index++) {
+        std::cout << index << "\n";
+    }
+
+    for (int j = 10; j == 0; j--) {
+        std::cout << j << "\n";
+    }
+
+    return 0;
+}
