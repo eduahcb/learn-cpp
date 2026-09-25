1e6f0da12af81a787e6a339b9c7385f7d7333bb6
Eduardo Barbosa <dev_eduardo@hotmail.com>
2026-09-25 13:23:51 -0300

    update

---
+++ if-statements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if (age >= 18) {
+        std::cout << "Welcome to the site!";
+    } else {
+        std::cout << "You are not old enough to enter!";
+    }
+
+    return 0;
+}
---
+++ switches.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main() {
+
+    int month;
+    std::cout << "Enter the month (1-12): ";
+    std::cin >> month;
+
+    switch (month) {
+    case 1:
+        std::cout << "Its January";
+        break;
+    case 2:
+        std::cout << "Its February";
+        break;
+    case 3:
+        std::cout << "Its March";
+        break;
+    case 4:
+        std::cout << "Its April";
+        break;
+    case 5:
+        std::cout << "Its May";
+        break;
+    case 6:
+        std::cout << "Its June";
+        break;
+    case 7:
+        std::cout << "Its July";
+        break;
+    case 8:
+        std::cout << "Its August";
+        break;
+    case 9:
+        std::cout << "Its September";
+        break;
+    case 10:
+        std::cout << "Its October";
+        break;
+    case 11:
+        std::cout << "Its November";
+        break;
+    case 12:
+        std::cout << "Its December";
+        break;
+    default:
+        std::cout << "Its You must enter with valid number";
+    }
+
+    return 0;
+}
